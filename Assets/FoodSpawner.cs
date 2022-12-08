6fcdca7c3c88bf1a171953bb111e6b1e0cfb1d52
Hannah Sloan <hannah.ava.sloan@gmail.com>
2022-12-08 16:19:32 -0700

    Initial Commit

---
+++ Assets/FoodSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    // The prefab for the food
+    public GameObject foodPrefab;
+
+    // The area in which the food will be spawned
+    public Vector2 spawnArea = new Vector2(20, 15);
+
+    // The current food
+    GameObject food;
+
+    // Spawn a new piece of food
+    void SpawnFood()
+    {
+        // Spawn the food at a random location within the spawn area
+        int x = (int)Random.Range(-spawnArea.x, spawnArea.x);
+        int y = (int)Random.Range(-spawnArea.y, spawnArea.y);
+
+        // Create a new food prefab
+        food = (GameObject)Instantiate(foodPrefab,
+                                       new Vector2(x, y),
+                                       Quaternion.identity);
+    }
+
+    // Called when the script is first loaded
+    void Start()
+    {
+        // Spawn the initial food
+        SpawnFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Is the food still on the screen?
+        if (food == null)
+        {
+            // If not, spawn a new piece of food
+            SpawnFood();
+        }
+    }
+}
---
+++ Assets/Snake.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Snake : MonoBehaviour
+{
+    // The direction the snake is moving in
+    Vector2 dir = Vector2.right;
+
+    // The list of all the snake's body parts
+    List<Transform> tail = new List<Transform>();
+
+    // Did the snake collide with food?
+    bool ate = false;
+
+    // The snake's speed
+    public float speed = 0.1f;
+
+    public float speedIncrease = 0.4f;
+
+    // The prefab for the snake's body part
+    public GameObject tailPrefab;
+
+    public int lengthAdded = 1;
+
+    // The object pool for the snake's body parts
+    Queue<Transform> bodyPartsPool = new Queue<Transform>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move in the direction the snake is facing
+        transform.Translate(dir * speed * Time.deltaTime);
+
+        // Did the snake collide with food?
+        if (ate)
+        {
+            speed += speedIncrease;
+
+            for (int i = 0; i < lengthAdded; i++)
+            {
+                // Get a body part from the object pool
+                Transform g = GetBodyPartFromPool();
+
+                // Set the body part's position and rotation
+                g.position = transform.position;
+                g.rotation = Quaternion.identity;
+
+                // Add the new body part to the list
+                tail.Insert(0, g.transform);
+            }
+
+            // Reset the flag
+            ate = false;
+        }
+        // Otherwise, move the last body part to where the head was
+        else if (tail.Count > 0)
+        {
+            tail.Last().position = transform.position;
+
+            // Add the last body part to the front of the list
+            tail.Insert(0, tail.Last());
+
+            // Remove the last body part from the list
+            tail.RemoveAt(tail.Count - 1);
+        }
+
+        // Change the direction the snake is facing based on input
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+
+    // Gets a body part from the object pool
+    Transform GetBodyPartFromPool()
+    {
+        // If the object pool is empty, create a new body part
+        if (bodyPartsPool.Count == 0)
+        {
+            GameObject g = (GameObject)Instantiate(tailPrefab);
+            g.SetActive(false);
+            bodyPartsPool.Enqueue(g.transform);
+        }
+        // Get the first body part from the queue
+        Transform t = bodyPartsPool.Dequeue();
+        // Make sure the body part is active
+        t.gameObject.SetActive(true);
+        return t;
+    }
+
+    // Returns a body part to the object pool
+    void ReturnBodyPartToPool(Transform t)
+    {
+        // Make sure the body part is inactive
+        t.gameObject.SetActive(false);
+        // Add the body part to the end of the queue
+        bodyPartsPool.Enqueue(t.gameObject.transform);
+    }
+
+
+    // Called when the snake collides with something
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // Did the snake collide with food?
+        if (coll.name.StartsWith("FoodPrefab"))
+        {
+            // Set the ate flag to true
+            ate = true;
+
+            // Destroy the food
+            Destroy(coll.gameObject);
+        }
+        // Otherwise, the snake collided with itself or a wall
+        else
+        {
+            // Restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
